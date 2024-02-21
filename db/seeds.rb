5ac543d849b45ce121fec0f1ad2df88207edef66
CongInCA <captainchenn@163.com>
2024-02-20 21:31:52 -0600

    Add csv to db and use it

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,49 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-require 'faker'
+# require 'faker'
 
-# Create 
-Faker::Config.random = Random.new(42)
+# # Create 
+# Faker::Config.random = Random.new(42)
 
-# Create 676 products.
-676.times do
-Product.create(
-title: Faker::Commerce.product_name,
-description: Faker::Lorem.sentence,
-price: Faker::Commerce.price,
-stock_quantity: Faker::Number.between(from: 1, to: 100)
-)
+# # Create 676 products.
+# 676.times do
+# Product.create(
+# title: Faker::Commerce.product_name,
+# description: Faker::Lorem.sentence,
+# price: Faker::Commerce.price,
+# stock_quantity: Faker::Number.between(from: 1, to: 100)
+# )
+# end
+
+require "csv"
+
+Product.destroy_all
+Category.destroy_all
+
+# Find the csv file and parse it.
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+
+# headers true means it has header.
+products = CSV.parse(csv_data, headers: true)
+
+products.each do |product|
+  # Fetch product's data.
+  title = product['name']
+  description = product['description']
+  price = product['price']
+  stock_quantity = product['stock quantity']
+  category_name = product['category']
+  
+  # Create or find the category by name.
+   category = Category.find_or_create_by(name: category_name)
+
+  #  A convinient way to create new product with category: The category.products method returns a collection of products associated with that category. 
+  product = category.products.create(
+    title: title,
+    description: description,
+    price: price,
+    stock_quantity: stock_quantity
+  )
 end
