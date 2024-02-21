71aa3ae1c8e65b38710c41ec7ebc351e90d4fa60
CongInCA <captainchenn@163.com>
2024-02-20 19:58:32 -0600

    Create 676 products

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+# Create 
+Faker::Config.random = Random.new(42)
+
+# Create 676 products.
+676.times do
+Product.create(
+title: Faker::Commerce.product_name,
+description: Faker::Lorem.sentence,
+price: Faker::Commerce.price,
+stock_quantity: Faker::Number.between(from: 1, to: 100)
+)
+end
